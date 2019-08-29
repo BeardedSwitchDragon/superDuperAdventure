dafd849cc4ca2fb8e0e49984d9d23c65e48ed848
Geddy Mukama <geddymaria.mukama@gmail.com>
2019-08-29 18:51:27 +1000

    Added clothing file and added more functions to player

---
+++ superDuperAdventure/clothing.py
@@ -0,0 +1,11 @@
+from Player import .
+class Clothing():
+    self.__init__(Clothing)
+    self.snazz = 5
+    #Snazz is a feature that fluctuates the way prople interact with you
+    #With no clothing your snazz is 0
+    self.clothingHeat = 3
+
+class Basic(Clothing):
+    self.__init__(Basic, Clothing)
+    self.snazz = 7
--- superDuperAdventure/play.py
+++ superDuperAdventure/play.py
@@ -1,20 +1,22 @@
-
+from clothing import .
 
 class Player():
-    self.__init__(Player)
+    self.__init__(Player, clothingHeat)
     self.money = 0
     self.weapons = {}
     self.clothing = {"Basic": "Bowler Hat", "Basic": "Black Comfy Sweater", "Basic": "Baggy Jeans", "Basic": "Neon Red & Blue Sneakers"}
     self.items = {}
     self.health = 50
     self.hunger = 0
+    self.warmth = 50
     self.moves = 0
 
     def increaseMoney(self, increase):
         self.money += increase
         self.update()
     def increaseHunger(self):
-        self.hunger += 1
+        if self.hunger < 100:
+            self.hunger += 1
         if self.moves %= 25:
             self.hunger += 1
         if self.hunger >= 10:
@@ -23,3 +25,11 @@ class Player():
     def update(self):
         self.increaseHunger()
         self.moves += 1
+        if self.warmth >= 110 or self.warmth <= 25:
+            self.health -= (self.warmth*self.warmth)/20
+
+    def changeClothing(self, index, key, value):
+        self.clothing[index].key() = key
+        self.clothing.value()
+    def displayStats(self):
+        print(self.health + "/100", self.hunger + "/100")
